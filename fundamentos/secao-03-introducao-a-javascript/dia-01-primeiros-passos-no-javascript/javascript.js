7288570e217b34ce506dff8f62da611e191adfeb
MateusDDev <ms4811978@gmail.com>
2023-05-31 19:06:36 -0300

    Exercício 4

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/javascript.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/javascript.js
@@ -1,6 +1,7 @@
 let num1 = 18;
 let num2 = 25;
 let num3 = 32;
+triangulo = false;
 
 //Exercício 1
 /*
@@ -15,20 +16,32 @@ console.log(num1 % num2);
 /*
 function maiorNumero(number1, number2) {
     if(number1 > number2){
-        console.log(`${number1} é maior do que ${number2}`);
+        return(console.log(`${number1} é maior do que ${number2}`));
     }else if(number2 > number1){
-        console.log(`${number2} é maior do que ${number1}`);
+        return(console.log(`${number2} é maior do que ${number1}`));
     }
 }
-
 console.log(maiorNumero(num1, num2));
 */
 
 //Exercício 3
+/*
 if(num1 > num2 && num1 > num3){
     console.log(`${num1} é o maior número`);
 }else if(num2 > num1 && num2 > num3){
     console.log(`${num2} é o maior número`);
 }else if(num3 > num1 && num3 > num2){
     console.log(`${num3} é o maior número`);
+}
+*/
+
+//Exercício 4
+if(num1 + num2 + num3 === 180){
+    triangulo = true;
+    console.log('É um triângulo');
+}else if(num1 + num2 + num3 < 0){
+    console.log('Ângulo inválido');
+}else {
+    triangulo = false;
+    console.log('Não é um triângulo');
 }
