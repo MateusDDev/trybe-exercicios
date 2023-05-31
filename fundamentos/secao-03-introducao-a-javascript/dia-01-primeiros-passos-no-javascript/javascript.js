b1c309c22e6cba9675ae50f61406fcb27df81275
MateusDDev <ms4811978@gmail.com>
2023-05-31 18:55:22 -0300

    Exercício 2 e 3

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/javascript.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/javascript.js
@@ -1,8 +1,34 @@
 let num1 = 18;
 let num2 = 25;
+let num3 = 32;
 
+//Exercício 1
+/*
 console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
 console.log(num1 % num2);
+*/
+
+//Exercício 2
+/*
+function maiorNumero(number1, number2) {
+    if(number1 > number2){
+        console.log(`${number1} é maior do que ${number2}`);
+    }else if(number2 > number1){
+        console.log(`${number2} é maior do que ${number1}`);
+    }
+}
+
+console.log(maiorNumero(num1, num2));
+*/
+
+//Exercício 3
+if(num1 > num2 && num1 > num3){
+    console.log(`${num1} é o maior número`);
+}else if(num2 > num1 && num2 > num3){
+    console.log(`${num2} é o maior número`);
+}else if(num3 > num1 && num3 > num2){
+    console.log(`${num3} é o maior número`);
+}
