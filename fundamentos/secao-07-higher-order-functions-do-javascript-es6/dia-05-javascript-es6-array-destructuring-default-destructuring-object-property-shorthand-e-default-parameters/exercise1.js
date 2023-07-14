1398a73b6975290cc6bf804378d9e6b021df857a
MateusDDev <ms4811978@gmail.com>
2023-07-14 18:49:46 -0300

    Exercício 2

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercise1.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercise1.js
@@ -1,5 +1,5 @@
 const myList = [5, 2, 3];
 
-const swap = ([index1, index2, index3]) => [index3, index2, index1];
+const swap = ( [index1, index2, index3] ) => [index3, index2, index1];
 
 console.log(swap(myList));
